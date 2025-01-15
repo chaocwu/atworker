7ea01e5e4b01b611b7d802f2581e311396e9e03c
chaocw <chaoc.w@outlook.com>
2025-01-15 23:48:55 +0800

    base layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Search,
+  ChevronLeft,
+  ChevronRight,
+  ArrowRightLeft,
+} from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen grid grid-cols-[30%_1fr] p-8 gap-8 overflow-hidden">
+      <div className="flex flex-col p-8 gap-4 border border-base-300 rounded-box">
+        <div className="flex flex-col gap-2">
+          <div className="flex items-center gap-2">
+            <ArrowRightLeft className="h-5 w-5" />
+            <h1 className="text-xl font-bold">Excel 2 Allure Template</h1>
+          </div>
+          <p className="text-xs opacity-50">
+            按既定文本用例格式生成测试脚本 Allure 模板
+          </p>
+        </div>
+        <div className="grid grid-cols-[70%_1fr] gap-4">
+          <input type="file" className="file-input file-input-primary w-full" />
+          <button className="btn btn-secondary btn-dash">示例模板</button>
+        </div>
+        <div className="flex flex-col gap-4 flex-1">
+          <label className="input w-full">
+            <Search className="h-4 w-4" />
+            <input type="search" />
+          </label>
+          <div className="overflow-x-auto border border-base-300 rounded-box min-h-[100vh] flex-1 md:min-h-min">
+            <table className="table table-sm">
+              <thead>
+                <tr>
+                  <th>编号</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>Cy Ganderton</td>
+                </tr>
+                <tr>
+                  <td>Hart Hagerty</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+                <tr>
+                  <td>Brice Swyre</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="flex justify-end items-center gap-4">
+            <button className="btn btn-soft btn-neutral btn-square">
+              <ChevronLeft className="h-4 w-4" />
+            </button>
+            <button className="btn btn-soft btn-neutral btn-square">
+              <ChevronRight className="h-4 w-4" />
+            </button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="mockup-code flex flex-col border border-base-300 rounded-box bg-base-100">
+        <pre>
+          <code>npm i daisyui</code>
+        </pre>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
